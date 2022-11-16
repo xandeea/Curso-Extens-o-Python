2e72a0178c1b5070a197f1893414fe6898726be9
xandeea <65933948+xandeea@users.noreply.github.com>
2022-11-16 22:12:50 +0000

    Aula3

---
+++ Aula3-1.py
@@ -0,0 +1,26 @@
+print("Início da aula 3 (09/11/2023)")
+
+contador = 1
+
+# Exibir de 1 até 10 repetidamente
+while(contador <= 10):
+  print(contador)
+  contador = contador+1 #contador += 1
+
+
+# Laço for (python for = for each)
+fruta = "morango"
+print(fruta)
+
+fruta1 = "morango"
+fruta2 = "laranja"
+fruta3 = "pêra"
+
+#Lista
+frutas = ["morango", "laranja", "pêra"]
+
+
+#mostra todas
+print(frutas)
+#quero exibir apenas a 3a. fruta (pêra)
+print(frutas[2])
---
+++ Aula3-2.py
@@ -0,0 +1,41 @@
+#criação de funções
+
+preco = 1999.90
+
+#Calcular apenas 5% de imposto, guardar na variavel imposto e exibir na tela
+imposto = preco * 0.05
+print(imposto)
+
+preco2 = 100
+imposto2 = preco2 * 0.05
+print(imposto2)
+
+#Criar uma função calcular_imposto() que calcular um imposto de 5% e retorna a quem pediu...
+#Isso é a declaração da função (Como ela funciona)
+def calcular_imposto(preco_produto):
+  imposto = preco_produto * 0.07
+  return imposto
+
+#Aqui é o uso... aqui é imposto a calcular.. e exibir na tela
+preco = 299
+imposto = calcular_imposto(preco)
+print(f"Esse aqui é com a função (7%): {imposto}")
+
+#Explicação de variável local x global
+print(preco) #????
+preco_produto = 100
+print(preco_produto) #????
+
+
+#agora calcular imposto a alíquota agora é 7%
+
+valores = [1.99, 24.50, 78.27, 1515.5]
+# se eu quiser calcular o imposto destes quatro valores... e exibir na tela assim: "O imposto de .... é ...." (1o. preço, 2o. imposto)
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto(valor)}")
+
+
+#Declarar um função calcular_imposto_aliquota que recebe dois parâmetros: o preço do produto e a alíquota de imposto a ser aplicada e retorna o imposto calculado. Se a aliquota não for informada, utilize 7% como padrão.
+def calcular_imposto_aliquota(valor, aliquota=7):
+  imposto = valor * aliquota / 100
+  return imposto
